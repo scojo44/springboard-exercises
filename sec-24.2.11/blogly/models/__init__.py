a752dd025ff60a17dc8d04b419e402a0cecfd277
scojo44 <Scott@scojo.org>
2024-04-11 14:28:58 -0700

    24.2.11 Simplify Base model

--- sec-24.2.11/blogly/models/__init__.py
+++ sec-24.2.11/blogly/models/__init__.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from .base import Base
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
 
 db = SQLAlchemy(model_class=Base)
 
