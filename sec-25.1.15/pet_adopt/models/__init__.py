db091b7c229ca9d60ecf875aacb90d5c620a62f8
scojo44 <Scott@scojo.org>
2024-04-11 14:33:57 -0700

    25.1.15 Simplify Base model

--- sec-25.1.15/pet_adopt/models/__init__.py
+++ sec-25.1.15/pet_adopt/models/__init__.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from .base import Base
+
+class Base(DeclarativeBase):
+    pass
 
 db = SQLAlchemy(model_class=Base)
 
