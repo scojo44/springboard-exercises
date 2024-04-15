f1f076fc39b9519dbb78f148b08f53d28adc2897
scojo44 <Scott@scojo.org>
2024-04-15 16:24:46 -0700

    24.1.22 Move database helpers to mixin class

---
+++ sec-24.1.22/blogly/models/helper.py
@@ -0,0 +1,50 @@
+from . import db
+
+class DBHelperMixin:
+    """Common database methods.  Only make instances with subclasses."""
+    @classmethod
+    def get(cls, primary_key):
+        """Return one instance of a model using the primary key or None if it doesn't exist."""
+        return db.session.get(cls, primary_key)
+
+    @classmethod
+    def get_or_404(cls, primary_key):
+        """Return one instance of a model using the primary key or a 404 error if it doesn't exist."""
+        return db.get_or_404(cls, primary_key, description=f"{cls.__name__} {primary_key} doesn't exist.")
+
+    @classmethod
+    def get_all(cls, select = None):
+        """Return all instances of a model."""
+        if select is None:
+            select = db.select(cls)
+        return db.session.scalars(select).all()
+
+    def save(self):
+        """Save the user to the database.  Returns whether the save was successful.
+        When False, find out what happened with get_last_error()."""
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return True
+        except Exception as error:
+            db.session.rollback()
+            self.last_error = error
+            return False
+        
+    def delete(self):
+        """Delete a user from the database.  Returns whether the delete was successful.
+        When False, find out what happened with get_last_error()."""
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return True
+        except Exception as error:
+            db.session.rollback()
+            self.last_error = error
+            return False
+
+    def get_last_error(self):
+        """Call to get error details after a failure in save() or delete()."""
+        error = self.last_error
+        self.last_error = None
+        return error
--- sec-24.1.22/blogly/models/user.py
+++ sec-24.1.22/blogly/models/user.py
@@ -1,27 +1,11 @@
 """Models for Blogly."""
+from .helper import DBHelperMixin
 from . import db
 
 DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
 
-class User(db.Model):
+class User(DBHelperMixin, db.Model):
     """User model class."""
-    @classmethod
-    def get(cls, primary_key):
-        """Return one instance of a model using the primary key or None if it doesn't exist."""
-        return db.session.get(cls, primary_key)
-
-    @classmethod
-    def get_or_404(cls, primary_key):
-        """Return one instance of a model using the primary key or a 404 error if it doesn't exist."""
-        return db.get_or_404(cls, primary_key, description=f"{cls.__name__} {primary_key} doesn't exist.")
-
-    @classmethod
-    def get_all(cls, select = None):
-        """Return all instances of a model."""
-        if select is None:
-            select = db.select(cls)
-        return db.session.scalars(select).all()
-
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -38,33 +22,3 @@ class User(db.Model):
     def __repr__(self):
         """Return a nicer description of User"""
         return f"<User #{self.id}: {self.first_name} {self.last_name} {self.image_url}>"
-    
-    def save(self):
-        """Save the user to the database.  Returns whether the save was successful.
-        When False, find out what happened with get_last_error()."""
-        try:
-            db.session.add(self)
-            db.session.commit()
-            return True
-        except Exception as error:
-            db.session.rollback()
-            self.last_error = error
-            return False
-        
-    def delete(self):
-        """Delete a user from the database.  Returns whether the delete was successful.
-        When False, find out what happened with get_last_error()."""
-        try:
-            db.session.delete(self)
-            db.session.commit()
-            return True
-        except Exception as error:
-            db.session.rollback()
-            self.last_error = error
-            return False
-
-    def get_last_error(self):
-        """Call to get error details after a failure in save() or delete()."""
-        error = self.last_error
-        self.last_error = None
-        return error
