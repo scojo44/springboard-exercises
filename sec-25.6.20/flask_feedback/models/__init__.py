6db6f05e56d1138f406c4b99f40c3922998112f0
scojo44 <Scott@scojo.org>
2024-04-15 17:19:51 -0700

    Revert "Remove unnecessary type_annotation_map"
    This reverts commit ed1f9aed937040b0cf3532ddc1296a1548772441.

--- sec-25.6.20/flask_feedback/models/__init__.py
+++ sec-25.6.20/flask_feedback/models/__init__.py
@@ -1,7 +1,8 @@
 """Flask Feedback Models"""
 from typing import Annotated
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, registry
 
 # Aliases for length-limited strings
 str20 = Annotated[str, 20]
@@ -9,7 +10,13 @@ str50 = Annotated[str, 50]
 hashed_password = Annotated[str, 60]
 
 class Base(DeclarativeBase):
-    pass
+    registry = registry(
+        type_annotation_map={
+            str20: String(20),
+            str50: String(50),
+            hashed_password: String(60)
+        }
+    )
 
 db = SQLAlchemy(model_class=Base)
 
