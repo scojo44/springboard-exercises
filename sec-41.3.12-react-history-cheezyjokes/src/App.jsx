77dd544de1a0e1f84d94abec80c63a3fec9953c9
scojo44 <Scott@scojo.org>
2024-07-27 19:06:27 -0700

    41.3.12 React History CheeZJokes: Start

--- sec-41.3.12-react-history-cheezyjokes/src/App.jsx
+++ sec-41.3.12-react-history-cheezyjokes/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import JokeList from "./JokeList";
 import './App.css'
 
 function App() {
   return (
-    <>
-    </>
+    <div className="App">
+      <JokeList />
+    </div>
   )
 }
 
